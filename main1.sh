e4ef2c12db6bc31347ecead0c5548959d4ac5575
comalnik <gostencnikluka@gmail.com>
2022-01-06 00:24:56 +0100

    atom commit

--- main1.sh
+++ main1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo cp -f res/os-release /etc/os-release
 sudo hostnamectl set-hostname gentowo
+sudo cp -f res/os-release /etc/os-release
 sudo cp -f res/issue /etc/issue
+sudo cp -f res/gentowo-logo.png /usr/share/pixmaps/gentowo-logo.png
+sudo cp -f res/gentowo-logo-text.png /usr/share/pixmaps/gentowo-logo-text.png
--- main2.sh
+++ main2.sh
@@ -2,3 +2,6 @@
 sudo emerge app-misc/neofetch
 sudo emerge x11-misc/polybar
 sudo emerge x11-wm/i3
+cp -f res2/polybar/config .config/polybar/config
+cp -f res2/polybar/launch.sh .config/polybar/launch.sh
+cp -f res2/i3/config .config/i3/config
