df086c491f6aaa0c9729a6dc7cd07769a3086e2b
comalnik <gostencnikluka@gmail.com>
2022-01-06 23:47:43 +0100

    added hostname

--- main1.sh
+++ main1.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo hostnamectl set-hostname gentowo
+sudo cp -f res/hostname /etc/conf.d/hostname
 sudo cp -f res/os-release /etc/os-release
 sudo cp -f res/issue /etc/issue
-sudo cp -f res/gentowo-logo.png /usr/share/pixmaps/gentowo-logo.png
-sudo cp -f res/gentowo-logo-text.png /usr/share/pixmaps/gentowo-logo-text.png
